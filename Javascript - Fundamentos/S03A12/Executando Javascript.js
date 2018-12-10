d4a9c53a2a0aab0d5a0271deb971fa4ace2c00a2
leeomarques <leonardogm_16@hotmail.com>
2018-12-10 11:45:40 -0300

    Ultima inserção Aula Tipagem Fraca

---
+++ Javascript - Fundamentos/S03A12/Executando Javascript.js
@@ -0,0 +1,4 @@
+//Como executar JS de maneiras diferentes.
+
+//https://repl.it/
+//https://jsfiddle.net/
---
+++ Javascript - Fundamentos/S03A13/Comentários.js
@@ -0,0 +1,19 @@
+//Comentário de uma linha
+console.log('linha 1');
+
+/*
+Comentário de 
+Múltiplas 
+Linhas 
+*/
+console.log('linha 2');
+
+/*
+ * Comentário
+ * de
+ * Múltiplas
+ * Linhas
+ */
+console.log('linha 3');
+
+
